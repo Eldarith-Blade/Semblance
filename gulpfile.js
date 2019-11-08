3992be95da2c80c4f12c11212561a68f0124a011
Peter Ovenden <Peterovenden@outlook.com>
2019-11-08 07:27:23 +0000

    Sass_Update

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,23 @@ const gulp = require('gulp');
 const sass = require('gulp-sass');
 const browserSync = require('browser-sync').create();
 
-//compile scss into css
+//compile scss into css for Bootstrap customisation
 function style() {
-    return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss', 'source/scss/*.scss'])
-    .pipe(sass().on('error',sass.logError))
+    return gulp.src('node_modules/bootstrap/scss/bootstrap.scss')
+    .pipe(sass())
     .pipe(gulp.dest('source/css'))
     .pipe(browserSync.stream());
 }
 
-//Move required files to folder
+//compile scss into css for main style or wildcard files
+function mStyle() {
+    return gulp.src('source/scss/style.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('source/css'))
+    .pipe(browserSync.stream());
+}
+
+//Move required files to folder for Bootstrap
 function compileJS() {
     return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js', 'node_modules/tether/dist/js/tether.min.js'])
     .pipe(gulp.dest("source/javascript"))
@@ -26,10 +34,11 @@ function watch() {
            index: "index.html"
         }
     });
-    gulp.watch('source/scss/**/*.scss', style)
+    gulp.watch('source/scss/*.scss', style)
     gulp.watch('./*.html').on('change',browserSync.reload);
     gulp.watch('./js/*.js').on('change', browserSync.reload);
 }
 exports.style = style;
 exports.watch = watch;
 exports.compileJS = compileJS;
+exports.mStyle = mStyle;
